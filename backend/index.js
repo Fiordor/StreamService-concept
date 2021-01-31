778ea5459fdddc8a08868292fe5f4fa1cc1e48d9
fiordor <1fiordor@gmail.com>
2021-01-31 18:08:49 +0100

    Complete naruto json, and post ready to rewrite

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded( { extended : true } ));
 app.use(bodyParser.json());
 
+
 const backendGET = require('./backendGET');
 const backendPOST = require('./backendPOST');
 
